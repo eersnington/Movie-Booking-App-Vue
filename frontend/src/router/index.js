6adc0347bf0e7505397a7c557bd0938256341704
Eers <sreeaadhi07@gmail.com>
2023-08-07 06:04:09 +0530

    Signup and added other files
    Implemented signup and added bunch of other files

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import LoginView from "../views/LoginView.vue";
+import SignupView from "../views/SignupView.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/login",
     name: "login",
     component: LoginView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
   },
 ];
 
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,21 +1,38 @@
 <template>
-    <div class="d-flex justify-content-center align-items-center">
-        <h2>Login</h2>
-        <form @submit.prevent="login">
-            <div class="form-group">
-                <label for="email">Username</label>
-                <input type="text" class="form-control" v-model="email" required>
+    <form @submit.prevent="login">
+        <div class="container">
+            <h1 class="h3">Please sign in</h1>
+            <div class="row">
+                <div class="col-sm">
+                    <div class="form-floating">
+                    <input type="email" class="form-control" v-model="email" id="email" required>
+                    <label for="email">Email address</label>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" v-model="password" required>
+            <div class="row">
+                <div class="col-sm">
+                    <div class="form-floating">
+                    <input type="password" class="form-control" v-model="password" id="password" required>
+                    <label for="password">Password</label>
+                    </div>
+                </div>
             </div>
-            <div class="error">
-                <p v-if="status === 'failed'" class="error-message">{{ fail_message }}</p>
+        <div class="row">
+            <div class="col-sm">
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign In</button>
             </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+            <div class="col-sm">
+                <router-link to="/signup" class="w-100 btn btn-lg btn-outline-primary" role="button">Sign Up</router-link>
+            </div>
+        </div>
     </div>
+    
+    <br>
+    <div class="error">
+        <p v-if="status === 'failed'" class="error-message">{{ fail_message }}</p>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -52,6 +69,15 @@ export default {
 </script>
 
 <style scoped>
+
+.container {
+    max-width: 500px;
+    padding: 15px;
+    margin: 0 auto;
+}
+.row {
+    padding: 15px;
+}
 .error-message {
     color: red;
 }
